c4f2d103ad2417d091e4311ed7ee24eab000b4d6
Darvetter <115429769+Darvetter@users.noreply.github.com>
2022-12-29 11:45:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a._xzgvkIGCafz60K1FnWvZjqOSNHV1km212HI_pd-P-MVwL-n_Mizar2JUUM71fprFmD9PaMg_mmduPbu6wJ7Eneta3YhKnrV87LU5OJAALZx4NWLGntvKHBEiSu_VrdpnAsFfth6SqqXoDM05nRpud3W0VV0XZuwD4NarmWGUpRvr8nBbYR2UsriMh5dvWXRVlzj_GjTdBlxuAdXwjT3Hw' # токен пользователя
+comm_token = '4cn4snamBuNIt5ef3XT3' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '0000'
+db_name = 'bot_users'
